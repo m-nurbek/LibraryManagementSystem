9e8927cfbbb39d000452bd370526633db6408cf6
Nurbek <nurbek.malikov@nu.edu.kz>
2024-06-02 17:08:05 +0500

    FIX: removed logback.xml and just configured logging in application.properties

--- src/main/java/nurbek/librarymanagementsystem/LibraryManagementSystemApplication.java
+++ src/main/java/nurbek/librarymanagementsystem/LibraryManagementSystemApplication.java
@@ -1,13 +1,32 @@
 package nurbek.librarymanagementsystem;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @SpringBootApplication
+@Slf4j
 public class LibraryManagementSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryManagementSystemApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner runner(TaskScheduler taskScheduler) {
+        return args -> {
+            var task = taskScheduler.scheduleAtFixedRate(() -> log.info("Hello World!"), 1000);
+            Thread.sleep(5000);
+            task.cancel(true);
+        };
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ThreadPoolTaskScheduler();
+    }
 }
