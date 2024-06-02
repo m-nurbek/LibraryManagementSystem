9b46f49da613dd0522e1023d094fcb9e1062f109
Nurbek <nurbek.malikov@nu.edu.kz>
2024-06-02 05:03:38 +0500

    FIX: fixed the logger, so that it now prints to file in logs/library.log

--- src/main/java/nurbek/librarymanagementsystem/aop/LoggingAspect.java
+++ src/main/java/nurbek/librarymanagementsystem/aop/LoggingAspect.java
@@ -21,18 +21,18 @@ public class LoggingAspect {
     public void logBefore(JoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
         String methodName = joinPoint.getSignature().getName();
-        log.debug(">> Controller method: {}() is called with args: {}", methodName, args);
+        log.debug(">> CALLED METHOD: {}() with args: {}", methodName, args);
     }
 
     @AfterReturning(value = "controllerPointcut() || executeLogging()", returning = "result")
     public void logAfter(JoinPoint joinPoint, Object result) {
         String methodName = joinPoint.getSignature().getName();
-        log.debug("<< Controller method: {}() returned: {}", methodName, result);
+        log.debug("<< METHOD RETURNED: {}() result: {}", methodName, result);
     }
 
     @AfterThrowing(pointcut = "controllerPointcut() || executeLogging()", throwing = "exception")
     public void logException(JoinPoint joinPoint, Throwable exception) {
         String methodName = joinPoint.getSignature().getName();
-        log.debug("<< Controller method: {}() - {}", methodName, exception.getMessage());
+        log.debug("!! ERROR: {}() - {}", methodName, exception.getMessage());
     }
 }
